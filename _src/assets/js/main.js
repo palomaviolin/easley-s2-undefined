fd72dc6317321ed54008b0283651d4e86a047565
Paloma Ania <paloma.aniabenito@gmail.com>
2018-11-28 03:02:45 +0100

    Improve previous commit

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -6,8 +6,23 @@ var coll = document.getElementsByClassName("collapsible");
 var i;
 
 for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
+  coll[i].addEventListener("click", function () {
     this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+var coll = document.getElementsByClassName("collapsible__button--share");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active__button--share");
     var content = this.nextElementSibling;
     if (content.style.display === "block") {
       content.style.display = "none";
