ce6e95022906a7a7256931adb6df8c3bb58685a6
Paloma Ania <paloma.aniabenito@gmail.com>
2018-11-28 05:42:11 +0100

    Add 4th collapsible, the one for the create-card-button

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -16,18 +16,3 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
-
-var coll = document.getElementsByClassName("collapsible__button--share");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function () {
-    this.classList.toggle("active__button--share");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-}
