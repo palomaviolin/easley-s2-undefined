fb9c568407c1db4152945788d7570c321b99de46
maria-badanina <maria.badanina.s@gmail.com>
2018-12-02 23:22:02 +0100

    First try to make smooth accordion

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -16,3 +16,35 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
+
+
+// let mom = document.querySelector('.section2__list');
+// let daughter  = document.querySelector('.section2__list--collapsible');
+// let arrow = document.querySelector('.icon__arrow--card-page');
+// let collapse = document.querySelector('.collapsible');
+
+// mom.addEventListener('click', openFun);
+
+// function openFun(e) {
+//   console.log(e.target);
+//   e.target.classList.toggle('open');
+//   if (e.target.classList.contains('open')) {
+//     arrow.classList.add('up');
+//     // daughter.classList.add('up');
+//   } else {
+//     arrow.classList.remove('up');
+//     // daughter.classList.remove('up');
+//   }
+// }
+
+// let firstChild = document.querySelector('#first_child');
+// let secondChild = document.querySelector('#second_child');
+// let thirdChild = document.querySelector('#third_child');
+
+// firstChild.addEventListener('click', openFun);
+// secondChild.addEventListener('click', openFun);
+// thirdChild.addEventListener('click', openFun);
+
+// function openFun(e) {
+//   e.currentTarget.classList.toggle('open');
+// }
