756d27621523083579c7958eb5a3f809e698447b
maria-badanina <maria.badanina.s@gmail.com>
2018-12-04 19:58:49 +0100

    Create tab and arrow animation

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,50 +1,31 @@
 'use strict';
 
-console.log('>> Ready :)');
+const firstChild = document.querySelector('#first_child');
+const secondChild = document.querySelector('#second_child');
+const thirdChild = document.querySelector('#third_child');
+const create = document.querySelector('.button__create-card');
+const content = document.querySelector('.create__mesage');
 
-var coll = document.getElementsByClassName("collapsible");
-var i;
+firstChild.addEventListener('click', openFun);
+secondChild.addEventListener('click', openFun);
+thirdChild.addEventListener('click', openFun);
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function () {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
+function openFun(e) {
+  e.currentTarget.classList.toggle('active');
+  if (e.currentTarget.classList.contains('active')) {
+    e.currentTarget.parentElement.classList.add('open');
+  } else {
+    e.currentTarget.parentElement.classList.remove('open');
+  }
 }
 
+create.addEventListener('click', showLink);
 
-// let mom = document.querySelector('.section2__list');
-// let daughter  = document.querySelector('.section2__list--collapsible');
-// let arrow = document.querySelector('.icon__arrow--card-page');
-// let collapse = document.querySelector('.collapsible');
-
-// mom.addEventListener('click', openFun);
-
-// function openFun(e) {
-//   console.log(e.target);
-//   e.target.classList.toggle('open');
-//   if (e.target.classList.contains('open')) {
-//     arrow.classList.add('up');
-//     // daughter.classList.add('up');
-//   } else {
-//     arrow.classList.remove('up');
-//     // daughter.classList.remove('up');
-//   }
-// }
-
-// let firstChild = document.querySelector('#first_child');
-// let secondChild = document.querySelector('#second_child');
-// let thirdChild = document.querySelector('#third_child');
-
-// firstChild.addEventListener('click', openFun);
-// secondChild.addEventListener('click', openFun);
-// thirdChild.addEventListener('click', openFun);
-
-// function openFun(e) {
-//   e.currentTarget.classList.toggle('open');
-// }
+function showLink(e) {
+  create.classList.toggle('active');
+  if (e.currentTarget.classList.contains('active')) {
+    content.classList.add('show');
+  } else {
+    content.classList.remove('show');
+  }
+}
