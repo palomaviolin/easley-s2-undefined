90454300b87c45dfa47e27a836c51e0a190bb1f4
katia1802 <katia.rojas.sandoval@gmail.com>
2018-12-04 12:31:02 +0100

    JS Profile Card Fonts

--- _src/assets/js/card_colors.js
+++ _src/assets/js/card_colors.js
@@ -5,7 +5,6 @@ const grayPaletteElement = document.querySelector('#palette__gray');
 const profileDataElement = document.querySelector ('.profile__data');
 
 // Palette color selection color green
-
 function greenProfileDataHandler () { 
     profileDataElement.classList.add('green');
     profileDataElement.classList.remove('red', 'gray');
@@ -25,3 +24,33 @@ function grayProfileDataHandler () {
 greenPaletteElement.addEventListener('click', greenProfileDataHandler);
 redPaletteElement.addEventListener('click', redProfileDataHandler);
 grayPaletteElement.addEventListener('click', grayProfileDataHandler);
+
+//Font styles por profile card
+
+const ubuntu= document.querySelector('#font__option--1');
+const comicSans= document.querySelector('#font__option--2');
+const montserrat= document.querySelector('#font__option--3');
+
+const profileDataGroup = document.querySelector('.profile__data-group');
+
+function ubuntuProfileDataHandler () { 
+    profileDataGroup.classList.add('ubuntu');
+    profileDataGroup.classList.remove('comic','montserrat');
+}
+
+
+function comicProfileDataHandler () { 
+    profileDataGroup.classList.add('comic');
+    profileDataGroup.classList.remove('ubuntu','montserrat');
+}
+
+
+function montserratProfileDataHandler () { 
+    profileDataGroup.classList.add('montserrat');
+    profileDataGroup.classList.remove('ubuntu','comic');
+}
+
+ubuntu.addEventListener('click', ubuntuProfileDataHandler);
+comicSans.addEventListener('click', comicProfileDataHandler);
+montserrat.addEventListener('click', montserratProfileDataHandler);
+
