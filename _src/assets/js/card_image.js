2e8ebcab42aa612554cc5d36c14a4fc6155144b1
clara <clarisaharguindey@gmail.com>
2018-12-05 23:47:26 +0100

    fix the javascript for image customizer

---
+++ _src/assets/js/card_image.js
@@ -0,0 +1,30 @@
+////IMAGE 
+
+const fr = new FileReader();
+const uploadBtn = document.querySelector('.button__add--image');
+
+const fileField = document.querySelector('#img-selector');
+
+const profileImages = document.querySelectorAll('.profile__image');
+
+//get user image
+function getImage(e) {
+var myFile = e.currentTarget.files[0];
+fr.addEventListener('load', writeImage);
+fr.readAsDataURL(myFile);
+}
+
+//put image in both places; profile and preview. using "for of", we don't need to write index of array
+
+function writeImage() {
+for (const cardImage of profileImages) {
+cardImage.style.backgroundImage = `url(${fr.result})`;
+}
+}
+
+function fakeFileClick() {
+fileField.click();
+}
+
+fileField.addEventListener('change', getImage);
+uploadBtn.addEventListener('click', fakeFileClick);
