c22b2bc82835a503fcda211d2dd3918929207487
clara <clarisaharguindey@gmail.com>
2018-12-11 12:27:03 +0100

    indent autocomplete-form and change double ' for single ones

--- _src/assets/js/autocomplete-form.js
+++ _src/assets/js/autocomplete-form.js
@@ -6,9 +6,9 @@
 let fullNameInput = document.body.querySelector('#name-input');
 
 function updateFullName(event) {
-    console.log(event);
-    let fullNameLabel = document.body.querySelector('#name');
-    fullNameLabel.innerText = event.target.value;
+  console.log(event);
+  let fullNameLabel = document.body.querySelector('#name');
+  fullNameLabel.innerText = event.target.value;
 }
 
 fullNameInput.addEventListener('keyup', updateFullName);
@@ -20,9 +20,9 @@ let jobPositionInput = document.body.querySelector('#job-input');
 console.log('jobPositionInput', jobPositionInput); // This line is to view the element jobPositionInput in the console.
 
 function updateJobPosition(event) {
-    console.log(event);
-    let jobPositionLabel = document.body.querySelector('#job-card');
-    jobPositionLabel.innerText = event.target.value;
+  console.log(event);
+  let jobPositionLabel = document.body.querySelector('#job-card');
+  jobPositionLabel.innerText = event.target.value;
 }
 
 jobPositionInput.addEventListener('keyup', updateJobPosition);
@@ -32,35 +32,35 @@ jobPositionInput.addEventListener('keyup', updateJobPosition);
 
 //HTML Checkbox
 function myFunction() {
-    var checkBox = document.getElementById("myCheck");
-    var text = document.getElementById("html");
-    if (checkBox.checked == true) {
-        text.style.display = "block";
-    } else {
-        text.style.display = "none";
-    }
+  var checkBox = document.getElementById('myCheck');
+  var text = document.getElementById('html');
+  if (checkBox.checked == true) {
+    text.style.display = 'block';
+  } else {
+    text.style.display = 'none';
+  }
 }
 
 //CSS Checkbox 
 function myFunction2() {
-    var checkBox = document.getElementById("css-input");
-    var text = document.getElementById("css");
-    if (checkBox.checked == true) {
-        text.style.display = "block";
-    } else {
-        text.style.display = "none";
-    }
+  var checkBox = document.getElementById('css-input');
+  var text = document.getElementById('css');
+  if (checkBox.checked == true) {
+    text.style.display = 'block';
+  } else {
+    text.style.display = 'none';
+  }
 }
 
 //React Checkbox 
 function myFunction3() {
-    var checkBox = document.getElementById("react-input");
-    var text = document.getElementById("react");
-    if (checkBox.checked == true) {
-        text.style.display = "block";
-    } else {
-        text.style.display = "none";
-    }
+  var checkBox = document.getElementById('react-input');
+  var text = document.getElementById('react');
+  if (checkBox.checked == true) {
+    text.style.display = 'block';
+  } else {
+    text.style.display = 'none';
+  }
 }
 
 
@@ -69,10 +69,10 @@ function myFunction3() {
 let emailInput = document.body.querySelector('#email-input');
 
 function updateEmail(event) {
-    console.log(event);
-    let emailLabel = document.querySelector('#email-card');
-    console.log(emailLabel);
-    emailLabel.href = `mailto:${event.currentTarget.value}`;
+  console.log(event);
+  let emailLabel = document.querySelector('#email-card');
+  console.log(emailLabel);
+  emailLabel.href = `mailto:${event.currentTarget.value}`;
 }
 
 emailInput.addEventListener('keyup', updateEmail);
@@ -83,8 +83,8 @@ emailInput.addEventListener('keyup', updateEmail);
 let githubInput = document.body.querySelector('#github-input');
 
 function updateGithub(event) {
-    let githubLabel = document.querySelector('#github-card');
-    githubLabel.href = `https://${event.currentTarget.value}`;
+  let githubLabel = document.querySelector('#github-card');
+  githubLabel.href = `https://${event.currentTarget.value}`;
 }
 
 githubInput.addEventListener('keyup', updateGithub);
@@ -95,8 +95,8 @@ githubInput.addEventListener('keyup', updateGithub);
 let linkedinInput = document.body.querySelector('#linkedin-input');
 
 function updateLinkedin(event) {
-    let linkedinLabel = document.querySelector('#linkedin-card');
-    linkedinLabel.href = `https://www.${event.currentTarget.value}`;
+  let linkedinLabel = document.querySelector('#linkedin-card');
+  linkedinLabel.href = `https://www.${event.currentTarget.value}`;
 }
 
 linkedinInput.addEventListener('keyup', updateLinkedin);
@@ -107,10 +107,8 @@ linkedinInput.addEventListener('keyup', updateLinkedin);
 let telInput = document.body.querySelector('#telf_movil');
 
 function updateTelephone(event) {
-    let telLabel = document.querySelector('#tel-card');
-    telLabel.href = `tel:${event.currentTarget.value}`;
+  let telLabel = document.querySelector('#tel-card');
+  telLabel.href = `tel:${event.currentTarget.value}`;
 }
 
 telInput.addEventListener('keyup', updateTelephone);
-
-
