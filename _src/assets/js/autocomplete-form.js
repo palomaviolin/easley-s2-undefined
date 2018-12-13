d7d9200b830e023b96e7a31d66cb325a2c940e04
katia1802 <katia.rojas.sandoval@gmail.com>
2018-12-13 15:56:57 +0100

    Local Storage Telephone and Email

--- _src/assets/js/autocomplete-form.js
+++ _src/assets/js/autocomplete-form.js
@@ -95,9 +95,9 @@ let emailInput = document.body.querySelector('#email-input');
 function updateEmail(event) {
   console.log(event);
   let emailLabel = document.querySelector('#email-card');
-  console.log(emailLabel);
+  // console.log(emailLabel);
   emailLabel.href = `mailto:${event.currentTarget.value}`;
-  updateDataObject('email', event.currrentTarget.value);
+  updateDataObject('email', event.currentTarget.value);
   updateLocalStorage();
 }
 
